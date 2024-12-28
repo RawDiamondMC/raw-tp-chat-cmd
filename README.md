e749bc1bc72f52cc9acf48eb2ee4efaaa46e313c
RawDiamondMC <RawDiamondMC@outlook.com>
2024-12-28 21:09:26 +0800

    Almost finished

--- README.md
+++ README.md
@@ -1,9 +1,28 @@
-# Fabric Example Mod
+<div>
+  <img style="float: left" width="128" src="src/main/resources/assets/raw-tpchat/icon.png" alt="icon"/>
+  <h1>TP Chat Command</h1>
+</div>
 
-## Setup
+![Stars](https://img.shields.io/github/stars/RawDiamondMC/raw-tp-chat-cmd) ![Forks](https://img.shields.io/github/forks/RawDiamondMC/raw-tp-chat-cmd)
+![Issues](https://img.shields.io/github/issues/RawDiamondMC/raw-tp-chat-cmd) ![Closed](https://img.shields.io/github/issues-closed/RawDiamondMC/raw-tp-chat-cmd)
+![Closed PR](https://img.shields.io/github/issues-pr-closed/RawDiamondMC/raw-tp-chat-cmd) ![Version](https://img.shields.io/github/v/release/RawDiamondMC/raw-tp-chat-cmd)
+![Test Status](https://img.shields.io/github/actions/workflow/status/RawDiamondMC/raw-tp-chat-cmd/build.yml) ![Downloads](https://img.shields.io/github/downloads/RawDiamondMC/raw-tp-chat-cmd/total)
 
-For setup instructions please see the [fabric wiki page](https://fabricmc.net/wiki/tutorial:setup) that relates to the IDE that you are using.
+This is a simple client Fabric mod for Minecraft. It provides a
+command called `/tpchat`. You can use it to send clickable message
+to **EVERY ONLINE PLAYER** for teleporting. And it can be used to teleport
+between dimensions, e.g., overworld <-> nether.
 
-## License
+![screenshot](assets/screenshot.png)
 
-This template is available under the CC0 license. Feel free to learn from it and incorporate it in your own projects.
+## Usage
+
+Syntax: `/tpchat [<dimension>] [<location>] [<description>]`.
+
+## Acknowledgement
+
+- [Fabric](https://fabricmc.net) - Thanks for the wonderful
+  framework, documents and community.
+- [得意黑 Smiley Sans](https://atelier-anchor.com/typefaces/smiley-sans) - Thanks for the awesome font, it is used in the icon.
+- [VueTorrent](https://github.com/VueTorrent/VueTorrent) - Thanks for
+  the README title template.
---
+++ src/main/java/me/rawdiamondmc/tpchat/RawTpChat.java
@@ -0,0 +1,178 @@
+package me.rawdiamondmc.tpchat;
+
+import com.mojang.brigadier.Command;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.command.argument.DimensionArgumentType;
+import net.minecraft.command.argument.MessageArgumentType;
+import net.minecraft.command.argument.RotationArgumentType;
+import net.minecraft.command.argument.TextArgumentType;
+import net.minecraft.command.argument.Vec3ArgumentType;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.ClickEvent;
+import net.minecraft.text.HoverEvent;
+import net.minecraft.text.MutableText;
+import net.minecraft.text.Style;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import net.minecraft.util.math.Vec2f;
+import net.minecraft.util.math.Vec3d;
+import org.jetbrains.annotations.Nullable;
+
+public final class RawTpChat {
+    private RawTpChat() {
+    }
+
+    public static final ModInitializer MAIN = () -> {
+        CommandRegistrationCallback.EVENT.register(((dispatcher, commandRegistryAccess, registrationEnvironment) -> {
+            dispatcher.register(CommandManager.literal("tpchat")
+                    .executes(context -> {
+                        final ServerCommandSource source = context.getSource();
+                        if (source.isExecutedByPlayer()) {
+                            final ServerPlayerEntity player = source.getPlayer();
+                            assert player != null;
+                            final String dimensionId = player.getServerWorld().getDimensionEntry().getIdAsString();
+                            final Vec3d location = new Vec3d(player.getX(), player.getY(), player.getZ());
+                            execute(source, dimensionId, location, player.getYaw(), player.getPitch(), null);
+                            return Command.SINGLE_SUCCESS;
+                        }
+                        throw new IllegalStateException("<dimension> and <location> is required on the console!");
+                    })
+                    .then(CommandManager.argument("dimension", DimensionArgumentType.dimension())
+                            .then(CommandManager.argument("location", Vec3ArgumentType.vec3())
+                                    .executes(context -> {
+                                        final ServerCommandSource source = context.getSource();
+                                        final String dimensionId = DimensionArgumentType.getDimensionArgument(context, "dimension").getDimensionEntry().getIdAsString();
+                                        final Vec3d location = Vec3ArgumentType.getVec3(context, "location");
+                                        execute(source, dimensionId, location, null);
+                                        return Command.SINGLE_SUCCESS;
+                                    })
+                                    .then(CommandManager.argument("rotation", RotationArgumentType.rotation())
+                                            .executes(context -> {
+                                                final ServerCommandSource source = context.getSource();
+                                                final String dimensionId = DimensionArgumentType.getDimensionArgument(context, "dimension").getDimensionEntry().getIdAsString();
+                                                final Vec3d location = Vec3ArgumentType.getVec3(context, "location");
+                                                final Vec2f rotation = RotationArgumentType.getRotation(context, "rotation").getRotation(source);
+                                                execute(source, dimensionId, location, rotation.x, rotation.y, null);
+                                                return Command.SINGLE_SUCCESS;
+                                            })
+                                            .then(CommandManager.argument("text", TextArgumentType.text(commandRegistryAccess))
+                                                    .requires(source -> source.hasPermissionLevel(2))
+                                                    .executes(context -> {
+                                                        final ServerCommandSource source = context.getSource();
+                                                        final String dimensionId = DimensionArgumentType.getDimensionArgument(context, "dimension").getDimensionEntry().getIdAsString();
+                                                        final Vec3d location = Vec3ArgumentType.getVec3(context, "location");
+                                                        final Text text = TextArgumentType.getTextArgument(context, "text");
+                                                        final Vec2f rotation = RotationArgumentType.getRotation(context, "rotation").getRotation(source);
+                                                        execute(source, dimensionId, location, rotation.x, rotation.y, text);
+                                                        return Command.SINGLE_SUCCESS;
+                                                    })
+                                            )
+                                            .then(CommandManager.literal("msg")
+                                                    .then(CommandManager.argument("message", MessageArgumentType.message())
+                                                            .executes(context -> {
+                                                                final ServerCommandSource source = context.getSource();
+                                                                final String dimensionId = DimensionArgumentType.getDimensionArgument(context, "dimension").getDimensionEntry().getIdAsString();
+                                                                final Vec3d location = Vec3ArgumentType.getVec3(context, "location");
+                                                                final Vec2f rotation = RotationArgumentType.getRotation(context, "rotation").getRotation(source);
+                                                                final Text text = MessageArgumentType.getMessage(context, "message");
+                                                                execute(source, dimensionId, location, rotation.x, rotation.y, text);
+                                                                return Command.SINGLE_SUCCESS;
+                                                            })
+                                                    )
+                                            )
+                                    )
+                                    .then(CommandManager.argument("text", TextArgumentType.text(commandRegistryAccess))
+                                            .requires(source -> source.hasPermissionLevel(2))
+                                            .executes(context -> {
+                                                final ServerCommandSource source = context.getSource();
+                                                final String dimensionId = DimensionArgumentType.getDimensionArgument(context, "dimension").getDimensionEntry().getIdAsString();
+                                                final Vec3d location = Vec3ArgumentType.getVec3(context, "location");
+                                                final Text text = TextArgumentType.getTextArgument(context, "text");
+                                                execute(source, dimensionId, location, text);
+                                                return Command.SINGLE_SUCCESS;
+                                            })
+                                    )
+                                    .then(CommandManager.literal("msg")
+                                            .then(CommandManager.argument("message", MessageArgumentType.message())
+                                                    .executes(context -> {
+                                                        final ServerCommandSource source = context.getSource();
+                                                        final String dimensionId = DimensionArgumentType.getDimensionArgument(context, "dimension").getDimensionEntry().getIdAsString();
+                                                        final Vec3d location = Vec3ArgumentType.getVec3(context, "location");
+                                                        final Text text = MessageArgumentType.getMessage(context, "message");
+                                                        execute(source, dimensionId, location, text);
+                                                        return Command.SINGLE_SUCCESS;
+                                                    })
+                                            )
+                                    )
+                            )
+                    )
+                    .then(CommandManager.argument("text", TextArgumentType.text(commandRegistryAccess))
+                            .requires(source -> source.hasPermissionLevel(2))
+                            .executes(context -> {
+                                final ServerCommandSource source = context.getSource();
+                                if (source.isExecutedByPlayer()) {
+                                    final ServerPlayerEntity player = source.getPlayer();
+                                    assert player != null;
+                                    final String dimensionId = player.getServerWorld().getDimensionEntry().getIdAsString();
+                                    final Vec3d location = new Vec3d(player.getX(), player.getY(), player.getZ());
+                                    final Text text = TextArgumentType.getTextArgument(context, "text");
+                                    execute(source, dimensionId, location, player.getYaw(), player.getPitch(), text);
+                                    return Command.SINGLE_SUCCESS;
+                                }
+                                throw new IllegalStateException("<dimension> and <location> is required on the console!");
+                            })
+                    )
+                    .then(CommandManager.literal("msg")
+                            .then(CommandManager.argument("message", MessageArgumentType.message())
+                                    .executes(context -> {
+                                        final ServerCommandSource source = context.getSource();
+                                        if (source.isExecutedByPlayer()) {
+                                            final ServerPlayerEntity player = source.getPlayer();
+                                            assert player != null;
+                                            final String dimensionId = player.getServerWorld().getDimensionEntry().getIdAsString();
+                                            final Vec3d location = new Vec3d(player.getX(), player.getY(), player.getZ());
+                                            final Text text = MessageArgumentType.getMessage(context, "message");
+                                            execute(source, dimensionId, location, text);
+                                            return Command.SINGLE_SUCCESS;
+                                        }
+                                        throw new IllegalStateException("<dimension> and <location> is required on the console!");
+                                    })
+                            )
+                    )
+            );
+        }));
+    };
+
+    private static void execute(ServerCommandSource source, String dimensionId, Vec3d location, @Nullable Text text) {
+        MutableText mutableText = (MutableText) text;
+        if (mutableText == null)
+            mutableText = ((MutableText) Text.of("[")).append(Text.translatableWithFallback("dimension." + dimensionId.replace(':', '.'), dimensionId)).append(Text.of(" " + Math.round(location.x) + ", " + Math.round(location.y) + ", " + Math.round(location.y) + "]"));
+        final Style newStyle = mutableText.getStyle()
+                .withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Text.translatable("chat.coordinates.tooltip")))
+                .withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/execute in " + dimensionId + " run tp @s " + location.x + " " + location.y + " " + location.z))
+                .withColor(Formatting.YELLOW);
+        if (source.isExecutedByPlayer()) {
+            source.getServer().getPlayerManager().broadcast(Text.translatable("raw-tpchat.share.player", source.getPlayer().getName()).append(mutableText.setStyle(newStyle)), false);
+        } else {
+            source.getServer().getPlayerManager().broadcast(Text.translatable("raw-tpchat.share.server").append(mutableText.setStyle(newStyle)), false);
+        }
+    }
+
+    private static void execute(ServerCommandSource source, String dimensionId, Vec3d location, float yaw, float pitch, @Nullable Text text) {
+        MutableText mutableText = (MutableText) text;
+        if (mutableText == null)
+            mutableText = ((MutableText) Text.of("[")).append(Text.translatableWithFallback("dimension." + dimensionId.replace(':', '.'), dimensionId)).append(Text.of(" " + Math.round(location.x) + ", " + Math.round(location.y) + ", " + Math.round(location.y) + "]"));
+        final Style newStyle = mutableText.getStyle()
+                .withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Text.translatable("chat.coordinates.tooltip")))
+                .withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/execute in " + dimensionId + " run tp @s " + location.x + " " + location.y + " " + location.z + " " + yaw + " " + pitch))
+                .withColor(Formatting.YELLOW);
+        if (source.isExecutedByPlayer()) {
+            source.getServer().getPlayerManager().broadcast(Text.translatable("raw-tpchat.share.player", source.getPlayer().getName()).append(mutableText.setStyle(newStyle)), false);
+        } else {
+            source.getServer().getPlayerManager().broadcast(Text.translatable("raw-tpchat.share.server").append(mutableText.setStyle(newStyle)), false);
+        }
+    }
+}
